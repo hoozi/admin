2dd036be5746700a68245112eb40695eeba5fd5b
AngryPanda <287036406@qq.com>
2018-09-27 20:14:46 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <a href="#/about">about</a>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
